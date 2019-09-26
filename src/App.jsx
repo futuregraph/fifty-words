e4d9f21ebb56b86a98405571e10ce56c298e85d2
futuregraph <whereisthew@gmail.com>
2019-09-26 23:04:00 +0300

    * add new words * add words list refresh * add TalentCity logo * now you can donate * some refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,51 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {getWordsList} from './words'
+import Donation from './donate.jsx'
 
 const Word = ({word}) => <span className="words-list__item">{word}</span>
-const Instructions = () => <div className="instructions">
-    Представляйте образ каждого слова ярко, в цвете 🌈. С ощущениями прикосновений 👆, звуками👂, запахами👃, а где это возможно - и со вкусом 👅.
-    Если этот набор слов уже вам встречался – просто перезагрузите страницу
-</div>
+const WordsList = ({words, onClick}) => (<div className="words-list" onClick={onClick}>
+    {
+        words.map((word, i) =>
+            (<span><Word word={word}/>{i !== words.length - 1 ? ', ' : '.'}</span>)
+        )
+    }
+</div>)
+
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            words: getWordsList()
+        }
+    }
+
+    changeWords() {
+        this.setState({words: getWordsList()})
+    }
+
     render() {
-        const words = getWordsList()
+        const {words} = this.state
+
         return (
             <div className="app-wrapper">
-                <h1>Упражнение "Оживление 50 слов"</h1>
-                <Instructions/>
-                <div className="words-list">
-                    {
-                        words.map( (word, i) =>
-                            (<span><Word word={word}/>{i !== words.length - 1 ? ', ': '.'}</span>)
-                        )
-                    }
+                <div className="app-header">
+                    <a target="_blank" href="https://vk.com/talentcity1" className="gt-logo"><img alt="Город талантов"
+                                                                                                  src="https://pp.userapi.com/0eJL973_dcofGpT3Z20_gywRTmn45Y9NUaiXMg/kALatrZnCIs.jpg?ava=1"/></a>
+                    <p className="app-header-text">Оживление 50&nbsp;слов</p>
+                </div>
+                <div className="instructions">
+                    Представляйте образ каждого слова ярко, в цвете 🌈. С ощущениями прикосновений 👆, звуками👂,
+                    запахами👃, а где это возможно - и со вкусом 👅.
+                    Если этот набор слов уже вам встречался – просто нажмите на список
+                </div>
+                <WordsList words={words} onClick={this.changeWords.bind(this)}/>
+                <div className="app-footer">
+                    <Donation/>
                 </div>
             </div>
         );
     }
 }
+
 export default App;
---
+++ src/donate.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+class Donation extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { formVisibility: false }
+    }
+
+    toggleForm () {
+        this.setState({formVisibility: !this.state.formVisibility})
+    }
+
+    render () {
+
+        const {formVisibility} = this.state
+
+        return <div className="donation-wrapper">
+            <div className="donation-button" onClick={this.toggleForm.bind(this)}>
+                👍 Поддержать разработчика
+            </div>
+            <div className="donation-form" style={{display: formVisibility ? 'block' : 'none' }}>
+                <iframe
+                    src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=%D0%9D%D0%B0%20%D1%80%D0%B0%D0%B7%D0%B2%D0%B8%D1%82%D0%B8%D0%B5%20%D1%83%D0%BF%D1%80%D0%B0%D0%B6%D0%BD%D0%B5%D0%BD%D0%B8%D0%B9&targets-hint=&default-sum=200&button-text=14&hint=&successURL=&quickpay=shop&account=4100110480715715"
+                    width="300" height="224" frameBorder="0" allowTransparency="true" scrolling="no"></iframe>
+            </div>
+            </div>
+    }
+}
+export default Donation
